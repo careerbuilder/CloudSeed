4b83f2839daf3ebf10952fc94a0cf20b76e0de73
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-05-19 08:29:19 -0400

    no test method

--- routes/parts.js
+++ routes/parts.js
@@ -2,17 +2,6 @@ var express = require('express');
 var db = require('mongoskin').db('mongodb://localhost:27017/cloudseed');
 var router = express.Router();
 
-router.get('/api/test', function(req, res){
-  db.collection('testing').find({},{"_id":false}).toArray(function(err, results){
-    if(err){
-      console.log(err);
-      return res.send({Success: false, Error: err});
-    }
-    console.log(results);
-    return res.send(results);
-  });
-});
-
 router.get('/api/parts', function(req, res){
   db.collection('parts').find({Subpart:false},{"_id":false}).toArray(function(err, results){
     if(err){
