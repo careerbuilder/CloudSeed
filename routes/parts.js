aa6acf605da7733569998525254c6548c09c82af
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-05-20 14:10:05 -0400

    try out aws id

--- routes/parts.js
+++ routes/parts.js
@@ -1,6 +1,60 @@
 var express = require('express');
 var db = require('mongoskin').db('mongodb://localhost:27017/cloudseed');
+var aws = require('aws-sdk');
 var router = express.Router();
+
+router.get('/api/awsvalues/:awstype', function(req, res){
+  var ptype = req.params.awstype;
+  var region = req.query.region;
+  var ec2 = new aws.EC2({region:region});
+  if(ptype==='AWS::EC2::AvailabilityZone::Name'){
+    ec2.describeAvailabilityZones(params, function(err, data) {
+      if (err){
+        console.log(err, err.stack);
+        return res.send({Success: false, Error: err, Values:[]});
+      }
+      else{
+        var rval = []
+        for(var i=0; i<data.AvailabilityZones.length; i++){
+          rval.push(data.AvailabilityZones[i].ZoneName);
+        }
+        return res.send({Success:true, Values:rval});
+      }
+    });
+  }
+  else if(ptype==='AWS::EC2::Instance::Id'){
+    ec2.describeInstances(params, function(err, data) {
+      if (err){
+        console.log(err, err.stack);
+        return res.send({Success: false, Error: err, Values:[]});
+      }
+      else{
+        return res.send({Success:true, Values:data});
+      }
+    });
+  }
+  else if(ptype==='AWS::EC2::Image::Id'){}
+  else if(ptype==='AWS::EC2::KeyPair::KeyName'){}
+  else if(ptype==='AWS::EC2::SecurityGroup::GroupName' || ptype==='AWS::EC2::SecurityGroup::Id'){
+    ec2.describeSecurityGroups(params, function(err, data) {
+      if (err){
+        console.log(err, err.stack);
+        return res.send({Success: false, Error: err, Values:[]});
+      }
+      else{
+        var rval = []
+        for(var i=0; i<data.SecurityGroups.length; i++){
+          rval.push(data.Reservations.Instances[i].Tags);
+        }
+        return res.send({Success:true, Values:rval});
+      }
+    });
+  }
+  else if(ptype==='AWS::EC2::Subnet::Id'){}
+  else if(ptype==='AWS::EC2::Volume::Id'){}
+  else if(ptype==='AWS::EC2::VPC::Id'){}
+  else{return res.send([])}
+});
 
 router.get('/api/parts', function(req, res){
   db.collection('parts').find({Subpart:false},{"_id":false}).toArray(function(err, results){
