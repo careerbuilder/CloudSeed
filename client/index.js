8f91ee509c8e44580220389ee3d9320ef29acdf4
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-08-24 14:05:40 -0400

    Do not need a welcome page

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *     http://www.apache.org/licenses/LICENSE-2.0
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,8 +16,11 @@ var app = angular.module('cloudseed', ['ngRoute', 'ngCookies', 'toastr']);
 
 app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
 	$routeProvider.when('/', {
-		controller: 'PageController',
-		templateUrl: 'views/welcome.html'
+		controller: 'PartCtrl',
+		templateUrl: 'views/parts.html',
+		resolve:{
+			auth: ["authservice", function(authservice) {return authservice.hasAccess();}]
+		}
 	})
 	.when('/login', {
 		controller: 'LoginCtrl',
@@ -26,13 +29,6 @@ app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpPro
 	.when('/signup', {
 		controller: 'SignupCtrl',
 		templateUrl: 'views/signup.html'
-	})
-	.when('/parts', {
-		controller: 'PartCtrl',
-		templateUrl: 'views/parts.html',
-		resolve:{
-			auth: ["authservice", function(authservice) {return authservice.hasAccess();}]
-		}
 	})
 	.when('/config', {
 		controller: 'ConfigCtrl',
